ae3095aff72b6c17c378ad4c825b470a8ec3c57f
rewe999 <54740556+rewe999@users.noreply.github.com>
2022-03-18 18:38:56 +0100

    first

---
+++ express.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const fs = require('fs');
+const cors = require("cors");
+
+const app = express();
+const PORT = 8000;
+
+const data = fs.readFileSync('ques.json')
+let questions = JSON.parse(data)    
+let dlugosc = questions.length
+
+app.use(express.json())
+
+app.use(cors());
+
+app.get('/', function(req, res) {
+  let random = Math.floor(Math.random() * dlugosc)
+  res.send({
+    pytanie: questions[random].pytanie,
+    odpowiedz: questions[random].odpowiedz
+  });
+});
+
+app.post('/', function(req, res) {
+  const { body } = req.body
+  res.send({
+    cialo: `twoje cialo ${body}`
+  })
+});
+
+app.listen(PORT, function() {
+  console.log(`Example app listening on port ${PORT}!`);
+});
