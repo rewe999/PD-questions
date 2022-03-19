0f5fc32e94f1947172601375521a8f4a51490852
rewe999 <54740556+rewe999@users.noreply.github.com>
2022-03-19 10:53:51 +0100

    new endpoint

--- express.js
+++ express.js
@@ -1,20 +1,21 @@
 const express = require('express');
 const fs = require('fs');
 const cors = require("cors");
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
 const data = fs.readFileSync('ques.json')
-let questions = JSON.parse(data)    
+let questions = JSON.parse(data)
 let dlugosc = questions.length
 
 app.use(express.json())
 
 app.use(cors());
 
-app.get('/', function(req, res) {
-  const hard = [4,25,26,28,30,42,45,47]
+app.get('/', function (req, res) {
+  const hard = [4, 25, 26, 28, 30, 42, 45, 47]
   let random = Math.floor(Math.random() * dlugosc)
   res.send({
     pytanie: questions[random].pytanie,
@@ -22,13 +23,19 @@ app.get('/', function(req, res) {
   });
 });
 
-app.post('/', function(req, res) {
-  const { body } = req.body
+app.get('/quest', function (req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.post('/', function (req, res) {
+  const {
+    body
+  } = req.body
   res.send({
     cialo: `twoje cialo ${body}`
   })
 });
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log(`Example app listening on port ${PORT}!`);
 });
